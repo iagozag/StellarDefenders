d0b9d20e8ad9a0cf911e0618bd17c74368288033
Denilson Martins Vieira Santos <denilsonmvs@gmail.com>
2025-06-25 20:07:54 -0300

    Necessário para compilar

--- Source/Actors/Laser.h
+++ Source/Actors/Laser.h
@@ -1,23 +1,23 @@
-//
-// Created by Lucas N. Ferreira on 12/09/23.
-//
+// //
+// // Created by Lucas N. Ferreira on 12/09/23.
+// //
 
-#pragma once
-#include "Actor.h"
+// #pragma once
+// #include "Actor.h"
 
-class Laser : public Actor
-{
-public:
-    Laser(class Game* game, float length = 10.0f, float deathTimer = 1.0f, bool isLaser = true);
+// class Laser : public Actor
+// {
+// public:
+//     Laser(class Game* game, float length = 10.0f, float deathTimer = 1.0f, bool isLaser = true);
 
-    void OnUpdate(float deltaTime) override;
+//     void OnUpdate(float deltaTime) override;
 
-private:
-    float mLength;
-    float mDeathTimer;
-    bool mIsLaser;
+// private:
+//     float mLength;
+//     float mDeathTimer;
+//     bool mIsLaser;
 
-    class DrawComponent* mDrawComponent;
-    class RigidBodyComponent* mRigidBodyComponent;
-    class CircleColliderComponent* mCircleColliderComponent;
-};
+//     class DrawComponent* mDrawComponent;
+//     class RigidBodyComponent* mRigidBodyComponent;
+//     class CircleColliderComponent* mCircleColliderComponent;
+// };
