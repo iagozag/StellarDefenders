f1ed18167ff40fa5c3bc09e2b32b525ed2466c5e
Denilson Martins Vieira Santos <denilsonmvs@gmail.com>
2025-06-25 20:02:44 -0300

    Alterando gitignore e CMakeLists

--- cmake-build-debug/CMakeFiles/stellar-defenders.dir/Source/MainMenu.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/stellar-defenders.dir/Source/MainMenu.cpp.obj: \
- C:\Users\csxuser\Downloads\StellarDefenders\Source\MainMenu.cpp \
- C:\Users\csxuser\Downloads\StellarDefenders\Source\MainMenu.h
