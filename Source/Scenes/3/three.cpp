c03805437023614ad0e2b816f689f93bed1d8ae6
iagozag <iagozag@gmail.com>
2025-07-07 10:57:51 -0300

    improved level 3

--- Source/Scenes/3/three.cpp
+++ Source/Scenes/3/three.cpp
@@ -9,16 +9,16 @@ const float VELOCITY_SCALE_FACTOR = 0.3;
 const float GM_SCALE_FACTOR = sq(VELOCITY_SCALE_FACTOR);
 
 const std::array<Planet, 5> planets = {{
-    Planet({-0.5, 0}, glm::vec2(0,  0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR, 0.12),
-    Planet({ 0, 0.1}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
-    Planet({ 0, -0.1}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
-    Planet({ 0.5, 0.5}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
-    Planet({ 0.5, -0.5}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR)
+    Planet({0, 0}, glm::vec2(0,  0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR, 0.12),
+    Planet({ 0.6, 0.6}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
+    Planet({ -0.6, -0.6}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
+    Planet({ 0.3, 0.3}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR),
+    Planet({ -0.3, -0.3}, glm::vec2(0, -0.1) * VELOCITY_SCALE_FACTOR, 0.03, 0.02 * GM_SCALE_FACTOR)
 }};
 
 const std::array<Target, 2> targets = {{
-    Target({0.44, 0.5}, glm::vec2(0, -0.67735) * VELOCITY_SCALE_FACTOR),
-    Target({0.44, -0.5}, glm::vec2(0, -0.67735) * VELOCITY_SCALE_FACTOR)
+    Target({0.78, 0.6}, glm::vec2(0, -0.67735) * VELOCITY_SCALE_FACTOR),
+    Target({-0.78, -0.6}, glm::vec2(0, -0.67735) * VELOCITY_SCALE_FACTOR)
 }};
 
 Level3::Level3(UIScreen *screen, Game &game):
